a99cbfd2ec64cb9b681d0b7fafbd3e62d0784014
Hammy <sgoudham@gmail.com>
2021-03-10 13:48:00 +0000

    Initial Commit

---
+++ src/main/java/org/somerville/Engine/App.java
@@ -0,0 +1,8 @@
+package org.somerville.Engine;
+
+public class App {
+
+    public static void main( String[] args ) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/org/somerville/AppTest.java
@@ -0,0 +1,15 @@
+package org.somerville;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
