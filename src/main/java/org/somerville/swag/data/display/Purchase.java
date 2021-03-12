8faa0ebdb077cf2b61fa35d23e3e34ae4b17fef4
Alan Robinson <alanrobinson1337@gmail.com>
2021-03-12 17:31:45 +0000

    dev/navigation: add purchase page for gui

---
+++ src/main/java/org/somerville/swag/data/display/Purchase.java
@@ -0,0 +1,41 @@
+package org.somerville.swag.data.display;
+
+import org.somerville.swag.data.entities.Customer;
+import org.somerville.swag.data.entities.Order;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Purchase {
+    private JTextField textField1;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JButton confirmButton;
+    private JButton backButton;
+    private JTextField textField2;
+    public JPanel root;
+
+    public Purchase(JFrame oldframe, Customer customer, Order order) {
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame frame = new JFrame("Somerville Swag");
+                frame.setContentPane(new Basket(oldframe, customer, order).root);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.pack();
+                frame.setVisible(true);
+                SwingUtilities.getWindowAncestor(root).dispose();
+            }
+        });
+
+        confirmButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                /**
+                 * needs to call card validation logic
+                 */
+            }
+        });
+    }
+}
