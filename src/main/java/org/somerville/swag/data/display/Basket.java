1688a8b56278a417e06df95452037f82ea451910
Alan Robinson <alanrobinson1337@gmail.com>
2021-03-12 17:31:32 +0000

    dev/navigation: add basket page for gui

---
+++ src/main/java/org/somerville/swag/data/display/Basket.java
@@ -0,0 +1,42 @@
+package org.somerville.swag.data.display;
+
+import org.somerville.swag.data.entities.Customer;
+import org.somerville.swag.data.entities.Order;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Basket {
+    private JTable tblBasket;
+    public JPanel root;
+    private JButton buyNowButton;
+    private JButton backButton;
+
+    public Basket(JFrame oldframe, Customer customer, Order order) {
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame frame = new JFrame("Somerville Swag");
+                frame.setContentPane(new LandingPage(oldframe, customer, order).root);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.pack();
+                frame.setVisible(true);
+                SwingUtilities.getWindowAncestor(root).dispose();
+            }
+        });
+
+        buyNowButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame frame = new JFrame("Somerville Swag");
+                frame.setContentPane(new Purchase(oldframe, customer, order).root);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.pack();
+                frame.setVisible(true);
+                SwingUtilities.getWindowAncestor(root).dispose();
+            }
+        });
+    }
+
+}
