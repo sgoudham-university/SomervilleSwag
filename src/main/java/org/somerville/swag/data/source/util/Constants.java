65524cd69dbb5d94681086918435fabfbb1cc671
hzl616 <suesskaninchen@gmail.com>
2021-03-14 15:04:27 +0000

    dev/database: add database constants

--- src/main/java/org/somerville/swag/data/source/util/Constants.java
+++ src/main/java/org/somerville/swag/data/source/util/Constants.java
@@ -5,4 +5,8 @@ public class Constants {
     public static final String TEST_RESOURCES_PATH = "src/test/resources/";
     public static final String LOG_FILE = "logs";
     public static final String DOT_TEXT = ".txt";
+
+    public static final String JDBC_URL = "jdbc:sqlite:";
+    public static final String DATABASE_URL = RESOURCES_PATH + "SomervilleSwagDB.db";
+
 }
