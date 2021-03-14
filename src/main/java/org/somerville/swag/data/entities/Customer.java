840562dd657c0c6071a7fffff3ffe2615073e61c
Alan Robinson <alanrobinson1337@gmail.com>
2021-03-14 15:28:12 +0000

    dev/designpatters: add state design pattern

--- src/main/java/org/somerville/swag/data/entities/Customer.java
+++ src/main/java/org/somerville/swag/data/entities/Customer.java
@@ -1,5 +1,46 @@
 package org.somerville.swag.data.entities;
 
-public class Customer {
+import org.somerville.swag.data.state.GuestState;
+import org.somerville.swag.data.state.LoggedInState;
+import org.somerville.swag.data.state.State;
+
+public class Customer  {
     //singleton and state design will need to go in here
+    private String Username;
+    private String Password;
+    private State state;
+
+    public Customer() {
+        this.state = new GuestState(this);
+    }
+
+    public Customer(String username, String password) {
+        this.Username = username;
+        this.Password = password;
+        this.state = new LoggedInState(this);
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
 }
