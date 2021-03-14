118f14deb0272a65224bc3c13d0663f35cf11e64
hzl616 <80228762+hzl616@users.noreply.github.com>
2021-03-14 16:04:34 +0000

    Remove FileWriterException import

--- src/main/java/org/somerville/swag/data/service/LoggingService.java
+++ src/main/java/org/somerville/swag/data/service/LoggingService.java
@@ -1,7 +1,5 @@
 package org.somerville.swag.data.service;
 
-import org.somerville.swag.data.exception.FileWriterException;
-
 public interface LoggingService {
     void logDatabaseWriteSuccess(String insertSQLStatement);
     void logDatabaseWriteFailure(String insertSQLStatement, String failureMessage);
