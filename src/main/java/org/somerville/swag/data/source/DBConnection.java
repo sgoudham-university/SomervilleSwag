e760d13ba4ee65c147b3a3b1915b89a115da1605
hzl616 <suesskaninchen@gmail.com>
2021-03-14 15:07:52 +0000

    dev/database: Update interface to reflect implementation

--- src/main/java/org/somerville/swag/data/source/DBConnection.java
+++ src/main/java/org/somerville/swag/data/source/DBConnection.java
@@ -1,7 +1,9 @@
 package org.somerville.swag.data.source;
 
+import org.somerville.swag.data.exception.DatabaseException;
+
 import java.sql.Connection;
 
 public interface DBConnection {
-    Connection connect();
+    Connection connect(String databaseUrl) throws DatabaseException;
 }
