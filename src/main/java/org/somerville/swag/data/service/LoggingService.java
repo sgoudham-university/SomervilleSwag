f664e5fa81dc359f2ef30149a19de1ed66a1cc79
hzl616 <suesskaninchen@gmail.com>
2021-03-14 15:34:25 +0000

    dev/database: Update interface to match implementation

--- src/main/java/org/somerville/swag/data/service/LoggingService.java
+++ src/main/java/org/somerville/swag/data/service/LoggingService.java
@@ -5,6 +5,8 @@ import org.somerville.swag.data.exception.FileWriterException;
 public interface LoggingService {
     void logDatabaseWriteSuccess(String insertSQLStatement) throws FileWriterException;
     void logDatabaseWriteFailure(String insertSQLStatement, String failureMessage) throws FileWriterException;
-    void logDatabaseReadSuccess( String selectSQLStatement) throws FileWriterException;
+    void logDatabaseReadSuccess(String selectSQLStatement) throws FileWriterException;
     void logDatabaseReadFailure(String selectSQLStatement, String failureMessage) throws FileWriterException;
+    void logDatabaseConnectSuccess(String successMessage) throws FileWriterException;
+    void logDatabaseConnectFailure(String databaseUrl, String failureMessage) throws FileWriterException;
 }
