60487e88dba06391c13e54f859f8a6fc5bf65913
hzl616 <suesskaninchen@gmail.com>
2021-03-14 15:52:11 +0000

    dev/database: Add logging service statements for database connection

--- src/main/java/org/somerville/swag/data/source/SQLiteConnection.java
+++ src/main/java/org/somerville/swag/data/source/SQLiteConnection.java
@@ -1,6 +1,10 @@
 package org.somerville.swag.data.source;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.somerville.swag.data.exception.DatabaseException;
+import org.somerville.swag.data.service.LoggingService;
+import org.somerville.swag.data.service.LoggingServiceImpl;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -14,6 +18,8 @@ public class SQLiteConnection implements DBConnection {
     private static SQLiteConnection instance;
     private Connection connection;
     private String databaseUrl = JDBC_URL + DATABASE_URL;
+
+    private static final LoggingService loggingService = LoggingServiceImpl.getInstance();
 
     private SQLiteConnection() throws DatabaseException {
         connect(databaseUrl);
@@ -30,7 +36,9 @@ public class SQLiteConnection implements DBConnection {
     public Connection connect(String databaseUrl) throws DatabaseException {
         try{
             connection = DriverManager.getConnection(databaseUrl);
+            loggingService.logDatabaseConnectSuccess("Connection successful to database: " + databaseUrl);
         } catch (SQLException sqle) {
+            loggingService.logDatabaseConnectFailure(databaseUrl, sqle.getMessage());
             throw new DatabaseException(sqle.getMessage(), sqle);
         }
         return connection;
