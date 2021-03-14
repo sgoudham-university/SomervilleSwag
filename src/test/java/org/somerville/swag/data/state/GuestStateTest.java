8ddd6630486e5478467e4f1a5faaae8915057a84
Alan Robinson <alanrobinson1337@gmail.com>
2021-03-14 16:02:41 +0000

    dev/designpatters: refactor test names

--- src/test/java/org/somerville/swag/data/state/GuestStateTest.java
+++ src/test/java/org/somerville/swag/data/state/GuestStateTest.java
@@ -6,16 +6,19 @@ import org.somerville.swag.data.entities.Customer;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GuestStateTest {
+    /**
+     * unsure if assert stmt is correct. Will look into
+     */
 
     @Test
-    void customerLoggedIn() {
+    void test_customerLoggedInState() {
         Customer c = new Customer("test","test");
         State s = new LoggedInState(c);
         assertEquals(c.getState(), s.customer.getState());
     }
 
     @Test
-    void testCustomerLoggedIn() {
+    void test_CustomerGuestState() {
         Customer c = new Customer();
         State s = new LoggedInState(c);
         assertEquals(c.getState(), s.customer.getState());
