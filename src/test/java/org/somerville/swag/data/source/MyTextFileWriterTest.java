f8af7387ee19cc8987386bf9298c24de4a6ff30a
Hammy <sgoudham@gmail.com>
2021-03-15 18:14:00 +0000

    dev/refactor-tests: Achieve 100% test coverage & Refactor methods/tests

--- src/test/java/org/somerville/swag/data/source/MyTextFileWriterTest.java
+++ src/test/java/org/somerville/swag/data/source/MyTextFileWriterTest.java
@@ -1,6 +1,6 @@
 package org.somerville.swag.data.source;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.somerville.swag.data.exception.FileWriterException;
 
@@ -10,57 +10,47 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.*;
 
 class MyTextFileWriterTest {
 
-    // Writing Tests mixing Mocks and Normal Implementations to Demonstrate Knowledge and Expertise
+    private MyFileWriter myFileWriter;
 
-    private static MyFileWriter mockedMyFileWriter;
-    private static MyFileWriter myFileWriter;
-
-    @BeforeAll
-    static void init() {
-        mockedMyFileWriter = mock(MyTextFileWriter.class);
+    @BeforeEach
+    void setup() {
         myFileWriter = new MyTextFileWriter("testLogs.txt", true);
     }
 
     @Test
-    void shouldSuccessfullyWriteToFile() {
+    void successfullyWriteToFile() {
         String expectedMessage = "testMessage";
         assertDoesNotThrow(() -> myFileWriter.writeToFile(expectedMessage, true));
     }
 
     @Test
-    void shouldSuccessfullyWriteToFileMocked() {
+    void failToWriteToFile() {
         String expectedMessage = "testMessage";
-        assertDoesNotThrow(() -> mockedMyFileWriter.writeToFile(expectedMessage, true));
-    }
+        String expectedInvalidFile = "src\\main\\resources";
 
-    @Test
-    void shouldFailToWriteToFileMocked() throws FileWriterException {
-        String expectedMessage = "testMessage";
-
-        String expectedErrorMessage = "FAILURE!";
+        String expectedErrorMessage = expectedInvalidFile + " (Access is denied)";
         FileWriterException expectedException = new FileWriterException(expectedErrorMessage, new IOException());
 
-        doThrow(expectedException).when(mockedMyFileWriter).writeToFile(expectedMessage, true);
+        myFileWriter.setFileToWrite(expectedInvalidFile);
+        Exception actualException = assertThrows(FileWriterException.class, () -> myFileWriter.writeToFile(expectedMessage, true));
 
-        Exception actualException = assertThrows(FileWriterException.class, () -> mockedMyFileWriter.writeToFile(expectedMessage, true));
-        assertThat(actualException.getMessage(), is(expectedErrorMessage));
+        assertThat(actualException.getMessage(), is(expectedException.getMessage()));
     }
 
     @Test
-    void shouldObtainCorrectLogPathWithConstructorArgs() {
+    void successfullyObtainLogPathWithConstructorArgs() {
         String expectedFilePath = "src/test/resources/testLogs.txt";
         assertThat(myFileWriter.getFileToWrite(), is(expectedFilePath));
     }
 
     @Test
-    void shouldObtainCorrectLogPathWithoutConstructorArgs() {
+    void successfullyObtainLogPathLogPathWithoutConstructorArgs() {
         String expectedFilePath = "src/main/resources/logs.txt";
-        MyFileWriter myFileWriter = new MyTextFileWriter();
 
+        MyFileWriter myFileWriter = new MyTextFileWriter();
         assertThat(myFileWriter.getFileToWrite(), is(expectedFilePath));
     }
 }
