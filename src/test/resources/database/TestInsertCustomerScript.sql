86e3d0c29be9ca144731cd94d659862fbfa52b67
hzl616 <suesskaninchen@gmail.com>
2021-03-15 23:10:58 +0000

    dev/database: Create test insert customer script

---
+++ src/test/resources/database/TestInsertCustomerScript.sql
@@ -0,0 +1,3 @@
+INSERT INTO Customer
+VALUES (1,'testForename','testSurname','testEmail@email.com','testPassword','testAddressLine1','testAddressLine2','testCity','testPostcode','testPhoneNumber' )
+;
