ad3af18fa0d71d4a7c380217ff75a5ba7978ae41
erinmaria98 <erinmaria98@users.noreply.github.com>
2021-03-15 15:43:30 +0000

    dev/database: Implement methods for select and insert statements.

---
+++ src/main/java/org/somerville/swag/data/source/SQLiteExecute.java
@@ -0,0 +1,40 @@
+package org.somerville.swag.data.source;
+
+import org.somerville.swag.data.exception.DatabaseException;
+import org.somerville.swag.data.exception.FileWriterException;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SQLiteExecute implements DBExecute {
+
+    private DBConnection connection;
+
+    public SQLiteExecute(DBConnection connection) throws SQLException {
+        this.connection = connection;
+    }
+
+    @Override
+    public ResultSet executeSelect(String selectQuery) {
+        ResultSet resultSet = null;
+        try (Connection connection = this.connection.connect()) {
+            Statement statement = connection.createStatement();
+            resultSet = statement.executeQuery(selectQuery);
+        } catch (DatabaseException | SQLException ioe) {
+            //placeholder exception to be thrown - EH
+        }
+        return resultSet;
+    }
+
+    @Override
+    public void executeInsert(String insertStatement) {
+        try (Connection connection = this.connection.connect()) {
+            Statement statement = connection.createStatement();
+            int rowsUpdated = statement.executeUpdate(insertStatement);
+        } catch (DatabaseException | SQLException ioe) {
+            //placeholder exception to be thrown - EH
+        }
+    }
+}
