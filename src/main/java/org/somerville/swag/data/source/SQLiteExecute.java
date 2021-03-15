930650517dd7f347ec1c1f0c022adfae6fc18be8
hzl616 <suesskaninchen@gmail.com>
2021-03-15 23:12:40 +0000

    dev/database: Implement methods for database read and write

--- src/main/java/org/somerville/swag/data/source/SQLiteExecute.java
+++ src/main/java/org/somerville/swag/data/source/SQLiteExecute.java
@@ -2,6 +2,8 @@ package org.somerville.swag.data.source;
 
 import org.somerville.swag.data.exception.SQLConnectionException;
 import org.somerville.swag.data.exception.SQLStatementException;
+import org.somerville.swag.data.service.LoggingService;
+import org.somerville.swag.data.service.LoggingServiceImpl;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -12,6 +14,8 @@ public class SQLiteExecute implements DBExecute {
 
     private DBConnection connection;
 
+    private LoggingService loggingService = LoggingServiceImpl.getInstance();
+
     public SQLiteExecute(DBConnection connection) throws SQLException {
         this.connection = connection;
     }
@@ -19,10 +23,12 @@ public class SQLiteExecute implements DBExecute {
     @Override
     public ResultSet executeSelect(String selectQuery) throws SQLStatementException {
         ResultSet resultSet = null;
-        try (Connection connection = this.connection.connect()) {
-            Statement statement = connection.createStatement();
+        try (Connection conn = connection.connect()) {
+            Statement statement = conn.createStatement();
             resultSet = statement.executeQuery(selectQuery);
+            loggingService.logDatabaseSelectSuccess(selectQuery);
         } catch (SQLConnectionException | SQLException err) {
+            loggingService.logDatabaseSelectFailure(selectQuery, err.getMessage());
             throw new SQLStatementException(err.getMessage(), err);
         }
         return resultSet;
@@ -30,10 +36,12 @@ public class SQLiteExecute implements DBExecute {
 
     @Override
     public void executeInsert(String insertStatement) throws SQLStatementException {
-        try (Connection connection = this.connection.connect()) {
-            Statement statement = connection.createStatement();
+        try (Connection conn = connection.connect()) {
+            Statement statement = conn.createStatement();
             int rowsUpdated = statement.executeUpdate(insertStatement);
+            loggingService.logDatabaseInsertSuccess(insertStatement, rowsUpdated);
         } catch (SQLConnectionException | SQLException err) {
+            loggingService.logDatabaseInsertFailure(insertStatement, err.getMessage());
             throw new SQLStatementException(err.getMessage(), err);
         }
     }
