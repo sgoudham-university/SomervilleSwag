af1a1734ae9eb0ef4d297231586c4eb008ba70de
Robert Ranson <robbieranson@me.com>
2021-03-15 15:00:09 +0000

    dev/entitypopulation Populated Order class & refactor Customer class

--- src/main/java/org/somerville/swag/data/entity/Customer.java
+++ src/main/java/org/somerville/swag/data/entity/Customer.java
@@ -4,20 +4,32 @@ import org.somerville.swag.data.state.Guest;
 import org.somerville.swag.data.state.LoggedIn;
 
 public class Customer  {
-    private String username;
+    private String email;
     private String password;
     private CustomerState customerState;
+    private Order currentOrder;
+    private int CustomerId;
+
+
 
     public Customer() {
         this.customerState = new Guest(this);
+        this.currentOrder = new Order();
     }
 
-    public Customer(String username, String password) {
-        this.username = username;
+    public Customer(String email, String password) {
+        this.email = email;
         this.password = password;
         this.customerState = new LoggedIn(this);
+
+    }
+    public int getCustomerId() {
+        return CustomerId;
     }
 
+    public void setCustomerId(int customerId) {
+        CustomerId = customerId;
+    }
     public void changeCustomerState(CustomerState customerState) {
         this.customerState = customerState;
     }
@@ -54,12 +66,12 @@ public class Customer  {
         customerState.purchaseItems();
     }
 
-    public String getUsername() {
-        return username;
+    public String getEmail() {
+        return email;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {
