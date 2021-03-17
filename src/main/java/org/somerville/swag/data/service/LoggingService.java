5fe3b06ca35ae3df12c8b5ea6bbce9c3a3733ba7
erinmaria98 <erinmaria98@users.noreply.github.com>
2021-03-17 21:01:30 +0000

    dev/database: Change order and add logging methods

--- src/main/java/org/somerville/swag/data/service/LoggingService.java
+++ src/main/java/org/somerville/swag/data/service/LoggingService.java
@@ -1,10 +1,14 @@
 package org.somerville.swag.data.service;
 
 public interface LoggingService {
+    void logDatabaseConnectSuccess(String successMessage);
+    void logDatabaseConnectFailure(String databaseUrl, String failureMessage);
+    void logDatabaseCreateTablesSuccess(String fileName);
+    void logDatabaseCreateTablesFailure(String fileName, String failureMessage);
+    void logDatabasePopulateTablesSuccess(String fileName);
+    void logDatabasePopulateTablesFailure(String fileName, String failureMessage);
     void logDatabaseInsertSuccess(String insertStatement, int rowsUpdated);
     void logDatabaseInsertFailure(String insertStatement, String failureMessage);
     void logDatabaseSelectSuccess(String selectQuery);
     void logDatabaseSelectFailure(String selectQuery, String failureMessage);
-    void logDatabaseConnectSuccess(String successMessage);
-    void logDatabaseConnectFailure(String databaseUrl, String failureMessage);
 }
