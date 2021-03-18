7bbd4a205b63bd5e5e047dd319929eda42e2fe8a
hzl616 <suesskaninchen@gmail.com>
2021-03-18 22:14:36 +0000

    dev/datetimeformatter: Migrate Clock to class from interface

--- src/main/java/org/somerville/swag/data/service/util/Clock.java
+++ src/main/java/org/somerville/swag/data/service/util/Clock.java
@@ -1,15 +1,5 @@
 package org.somerville.swag.data.service.util;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-public class Clock {
-
-    private String pattern = "yyyy-MM-dd HH:mm:ss";
-
-    public String getCurrentTime(){
-        LocalDateTime localDateTime = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
-        return localDateTime.format(formatter);
-    }
+public interface Clock {
+    String getCurrentTime();
 }
