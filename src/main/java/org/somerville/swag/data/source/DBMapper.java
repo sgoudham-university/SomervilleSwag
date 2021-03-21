847319420e23c12ab85bdbd3ee7fcdb27ecf3073
Hammy <sgoudham@gmail.com>
2021-03-21 23:36:17 +0000

    [A1] dev/database: Map customer details from ResultSet

--- src/main/java/org/somerville/swag/data/source/DBMapper.java
+++ src/main/java/org/somerville/swag/data/source/DBMapper.java
@@ -3,13 +3,33 @@ package org.somerville.swag.data.source;
 import org.somerville.swag.data.entity.Customer;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DBMapper {
 
-    DBMapper() { }
+    public DBMapper() { }
 
-    public Customer mapToCustomer(ResultSet customerData) {
-
-        return null;
+    public Customer mapToCustomer(ResultSet customerData, Customer customer) {
+        try {
+            if (!customerData.isBeforeFirst()) {
+                System.out.println("hi");
+            } else {
+                while (customerData.next()) {
+                    customer.setCustomerId(customerData.getInt("CustomerId"));
+                    customer.setForename(customerData.getString("Forename"));
+                    customer.setSurname(customerData.getString("Surname"));
+                    customer.setEmail(customerData.getString("Email"));
+                    customer.setPassword(customerData.getString("Password"));
+                    customer.setAddressLine1(customerData.getString("AddressLine1"));
+                    customer.setAddressLine2(customerData.getString("AddressLine2"));
+                    customer.setCity(customerData.getString("City"));
+                    customer.setPostcode(customerData.getString("Postcode"));
+                    customer.setPhoneNumber(customerData.getString("PhoneNumber"));
+                }
+            }
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+        return customer;
     }
 }
