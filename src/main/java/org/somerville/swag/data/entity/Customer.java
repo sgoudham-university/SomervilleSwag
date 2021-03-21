7a347ad3bac0c6dc3de0ed1549bf06534ad40dde
Hammy <sgoudham@gmail.com>
2021-03-21 23:25:57 +0000

    dev/database: Reformat file

--- src/main/java/org/somerville/swag/data/entity/Customer.java
+++ src/main/java/org/somerville/swag/data/entity/Customer.java
@@ -4,13 +4,13 @@ import org.somerville.swag.data.entity.state.Guest;
 import org.somerville.swag.data.entity.state.LoggedIn;
 
 public class Customer {
-    private String email;
-    private String password;
     private CustomerState customerState;
     private Order currentOrder;
     private int customerId;
     private String forename;
     private String surname;
+    private String email;
+    private String password;
     private String addressLine1;
     private String addressLine2;
     private String city;
@@ -24,9 +24,9 @@ public class Customer {
     }
 
     public Customer(String email, String password, Order order) {
+        this.customerState = new LoggedIn(this);
         this.email = email;
         this.password = password;
-        this.customerState = new LoggedIn(this);
         this.currentOrder = order;
 
     }
@@ -34,9 +34,7 @@ public class Customer {
     public Customer(String email, String password, Order currentOrder, int customerId, String forename,
                     String surname, String addressLine1, String addressLine2, String city, String postcode,
                     String phoneNumber) {
-
         this.customerState = new LoggedIn(this);
-
         this.email = email;
         this.password = password;
         this.currentOrder = currentOrder;
@@ -52,10 +50,6 @@ public class Customer {
 
     public void changeCustomerState(CustomerState customerState) {
         this.customerState = customerState;
-    }
-
-    public CustomerState getCustomerState() {
-        return customerState;
     }
 
     public void signUp() {
@@ -86,83 +80,29 @@ public class Customer {
         customerState.purchaseItems();
     }
 
-    public String getEmail() {
-        return email;
-    }
+    public void setCurrentOrder(Order currentOrder) { this.currentOrder = currentOrder; }
+    public void setCustomerId(int customerId) { this.customerId = customerId; }
+    public void setForename(String forename) { this.forename = forename; }
+    public void setSurname(String surname) { this.surname = surname; }
+    public void setEmail(String email) { this.email = email; }
+    public void setPassword(String password) { this.password = password; }
+    public void setAddressLine1(String addressLine1) { this.addressLine1 = addressLine1; }
+    public void setAddressLine2(String addressLine2) { this.addressLine2 = addressLine2; }
+    public void setCity(String city) { this.city = city; }
+    public void setPostcode(String postcode) { this.postcode = postcode; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public int getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(int customerId) {
-        this.customerId = customerId;
-    }
-
-    public String getForename() {
-        return forename;
-    }
-
-    public void setForename(String forename) {
-        this.forename = forename;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getAddressLine1() {
-        return addressLine1;
-    }
-
-    public void setAddressLine1(String addressLine1) {
-        this.addressLine1 = addressLine1;
-    }
-
-    public String getAddressLine2() {
-        return addressLine2;
-    }
-
-    public void setAddressLine2(String addressLine2) {
-        this.addressLine2 = addressLine2;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getPostcode() {
-        return postcode;
-    }
-
-    public void setPostcode(String postcode) {
-        this.postcode = postcode;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+    public CustomerState getCustomerState() { return customerState; }
+    public Order getCurrentOrder() { return currentOrder; }
+    public int getCustomerId() { return customerId; }
+    public String getForename() { return forename; }
+    public String getSurname() { return surname; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+    public String getAddressLine1() { return addressLine1; }
+    public String getAddressLine2() { return addressLine2; }
+    public String getCity() { return city; }
+    public String getPostcode() { return postcode; }
+    public String getPhoneNumber() { return phoneNumber; }
 }
+
