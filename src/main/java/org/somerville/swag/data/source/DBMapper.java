3acbce68f3158d4896ee2eec442860a5c29c2f34
Hammy <sgoudham@gmail.com>
2021-03-21 21:51:00 +0000

    dev/database: Stub out DBMapper.java

---
+++ src/main/java/org/somerville/swag/data/source/DBMapper.java
@@ -0,0 +1,15 @@
+package org.somerville.swag.data.source;
+
+import org.somerville.swag.data.entity.Customer;
+
+import java.sql.ResultSet;
+
+public class DBMapper {
+
+    DBMapper() { }
+
+    public Customer mapToCustomer(ResultSet customerData) {
+
+        return null;
+    }
+}
