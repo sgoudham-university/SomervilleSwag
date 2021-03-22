baba8545400b2ec46340af069c81e7f249ffb402
hzl616 <suesskaninchen@gmail.com>
2021-03-22 01:33:23 +0000

    [A1] dev/database: Update return value

--- src/main/java/org/somerville/swag/data/source/DBExecute.java
+++ src/main/java/org/somerville/swag/data/source/DBExecute.java
@@ -6,5 +6,5 @@ import java.sql.ResultSet;
 
 public interface DBExecute {
     ResultSet executeSelect(String selectQuery) throws SQLStatementException;
-    void executeInsert(String insertStatement) throws SQLStatementException;
+    int executeInsert(String insertStatement) throws SQLStatementException;
 }
