0748713e40a3e5390e59aa26b8e4836433837d78
Hammy <sgoudham@gmail.com>
2021-03-22 22:42:44 +0000

    dev/database: Add logging for mapToCustomer() & add mapToProducts() method

--- src/main/java/org/somerville/swag/data/source/DBMapper.java
+++ src/main/java/org/somerville/swag/data/source/DBMapper.java
@@ -1,18 +1,24 @@
 package org.somerville.swag.data.source;
 
 import org.somerville.swag.data.entity.Customer;
+import org.somerville.swag.data.entity.Product;
+import org.somerville.swag.data.service.LoggingService;
+import org.somerville.swag.data.service.LoggingServiceImpl;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 public class DBMapper {
+
+    private LoggingService loggingService = LoggingServiceImpl.getInstance();
 
     public DBMapper() { }
 
     public void mapToCustomer(ResultSet customerData, Customer customer) {
         try {
             if (!customerData.isBeforeFirst()) {
-                // log customer not found
+                loggingService.logDatabaseCustomerNotFound();
             } else {
                 while (customerData.next()) {
                     customer.setCustomerId(customerData.getInt("CustomerId"));
@@ -27,9 +33,35 @@ public class DBMapper {
                     customer.setPhoneNumber(customerData.getString("PhoneNumber"));
                 }
             }
-            // log map success
+            loggingService.logDatabaseCustomerMapSuccess(customer.getCustomerId());
         } catch (SQLException sqle) {
-            // log map failure
+            loggingService.logDatabaseCustomerMapFailure(sqle.getMessage());
         }
+    }
+
+    public void mapToProducts(ResultSet allProductsData, List<Product> allProducts) {
+        try {
+            if (!allProductsData.isBeforeFirst()) {
+                loggingService.logDatabaseNoProductsInStock();
+            } else {
+                while (allProductsData.next()) {
+                    allProducts.add(new Product(
+                            allProductsData.getInt("ProductId"),
+                            allProductsData.getString("Name"),
+                            allProductsData.getString("Description"),
+                            allProductsData.getBigDecimal("Price"),
+                            allProductsData.getInt("StockLevel"),
+                            allProductsData.getString("ImagePath")
+                    ));
+                }
+            }
+            loggingService.logDatabaseAllProductsMapSuccess();
+        } catch (SQLException sqle) {
+            loggingService.logDatabaseAllProductsMapFailure(sqle.getMessage());
+        }
+    }
+
+    public void setLoggingService(LoggingService loggingService) {
+        this.loggingService = loggingService;
     }
 }
