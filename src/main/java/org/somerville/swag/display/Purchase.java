1639dfdab9b733b45342334698b799cae6554d9f
Alan Robinson <alanrobinson1337@gmail.com>
2021-03-22 20:58:30 +0000

    [D2] dev/productDisplay Add values to combo boxes in Purchase Frame

--- src/main/java/org/somerville/swag/display/Purchase.java
+++ src/main/java/org/somerville/swag/display/Purchase.java
@@ -3,17 +3,29 @@ package org.somerville.swag.display;
 import org.somerville.swag.data.entity.Customer;
 
 import javax.swing.*;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Date;
 
 public class Purchase {
     private JTextField textField1;
-    private JComboBox comboBox1;
-    private JComboBox comboBox2;
+    private JComboBox comMonth;
+    private JComboBox comYear;
     private JButton confirmButton;
     private JButton backButton;
     private JTextField textField2;
     public JPanel root;
 
     public Purchase(JFrame oldFrame, Customer customer) {
+
+        int year = LocalDateTime.now().getYear();
+        for( int i = 0; i < 6; i++){
+            comYear.addItem(year + i);
+        }
+        for(int j = 0; j <= 12; j++){
+            comMonth.addItem( j );
+        }
+
         backButton.addActionListener(actionEvent -> {
             new JFrameBuilder.Builder().buildDefaultJFrame(new Basket(oldFrame, customer).root, true);
             SwingUtilities.getWindowAncestor(root).dispose();
