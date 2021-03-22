80b1328014f40f0fb3d4d314ab4a3a64a457650f
Hammy <sgoudham@gmail.com>
2021-03-22 18:11:11 +0000

    [C2] dev/productdisplay: Refactor LandingPage to include product image WIP

--- src/main/java/org/somerville/swag/display/LandingPage.java
+++ src/main/java/org/somerville/swag/display/LandingPage.java
@@ -5,14 +5,8 @@ import org.somerville.swag.data.entity.Order;
 import org.somerville.swag.data.entity.OrderLine;
 import org.somerville.swag.data.entity.Product;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
@@ -23,7 +17,7 @@ public class LandingPage {
     private JButton viewBasketButton;
     private JButton addToBasketButton;
     public JPanel root;
-    private JList listOfProducts;
+    private JList<Product> listOfProducts;
     private JPanel imagePanel;
     private JSpinner quantitySpinner;
     private JLabel lblDesc;
@@ -37,56 +31,25 @@ public class LandingPage {
          * Remove ASAP
          * Do not leave comment once removed
          */
-        DefaultListModel listModel = new DefaultListModel();
+        HashMap<Integer, Product> allProducts = getAllProducts();
+        displayProductList(allProducts, new DefaultListModel<>());
 
-        Product productA = new Product(0,"Product A","Description A", BigDecimal.valueOf(19.99), 5, "src/main/resources/product/images/derek_crocs.png");
-        Product productB = new Product(0,"Product B","Description B", BigDecimal.valueOf(29.99), 10, "src/main/resources/product/images/derek_snowglobe.png");
+        listOfProducts.addListSelectionListener(actionEvent -> {
+            Product selectedProduct = listOfProducts.getSelectedValue();
 
-        //-------------------- BEGIN LIST CREATION----------------------------------------------------------------------
+            SpinnerModel model = new SpinnerNumberModel(1, 1, selectedProduct.getStockLevel(), 1);
+            JSpinner spinner = new JSpinner();
+            JFormattedTextField spin = ((JSpinner.DefaultEditor) spinner.getEditor()).getTextField();
+            spin.setEditable(false);
+            quantitySpinner.setModel(model);
 
-        HashMap<Integer, Product> productMap = new HashMap<>(); //set this to the DB read fucntion
-        productMap.put(0, productA);
-        productMap.put(1, productB);
+            lblDesc.setText(selectedProduct.getDescription());
 
-        for (Map.Entry<Integer, Product> productEntry : productMap.entrySet()) {
-            Product product = productEntry.getValue();
-            if (product.getStockLevel() > 0 ){
-                listModel.addElement(product);
-            }
-        }
-        listOfProducts.setModel(listModel);
+            ImageIcon productImageIcon = new ImageIcon(selectedProduct.getImagepath());
+            Image scaledProductImage = productImageIcon.getImage().getScaledInstance(350, 400, Image.SCALE_SMOOTH);
+            imageDisplay.setIcon(new ImageIcon(scaledProductImage));
 
-        //---------------------END OF LIST CREATION--------------------------------------------------------------------
-
-    // -------------------- BEGIN DYNAMIC DISPLAY BLOCK ----------------------------------------------------------------
-        listOfProducts.addListSelectionListener(new ListSelectionListener() {
-            @Override
-            public void valueChanged(ListSelectionEvent e) {
-                Object selectedObject = (Object) listOfProducts.getSelectedValue();
-                Product selectedProduct = (Product) selectedObject;
-
-                SpinnerModel model = new SpinnerNumberModel(1,1,selectedProduct.getStockLevel(),1);
-                JSpinner spinner= new JSpinner();
-                JFormattedTextField spin=((JSpinner.DefaultEditor)spinner.getEditor()).getTextField();
-                spin.setEditable(false);
-                quantitySpinner.setModel(model);
-
-                lblDesc.setText(selectedProduct.getDescription());
-
-
-
-                ImageIcon icon = new ImageIcon(selectedProduct.getImagepath());
-
-                imageDisplay.setIcon(icon);
-
-
-//                ImageIcon productImage = createImageIcon(selectedProduct.getImagepath(),selectedProduct.getDescription());
-//                imageDisplay = new JLabel(productImage);
-
-
-            }
         });
-    // -------------------- BEGIN DYNAMIC DISPLAY BLOCK ----------------------------------------------------------------
 
         signUpButton.addActionListener(actionEvent -> {
             new JFrameBuilder.Builder().buildDefaultJFrame(new SignUp(oldFrame, customer).root,true);
@@ -104,13 +67,13 @@ public class LandingPage {
         });
 
         addToBasketButton.addActionListener(actionEvent -> customer.addProductToBasket());
+
         if(listOfProducts.getSelectedIndex() == -1){
             System.out.println("No product selected");
         } else {
             int quantity = (int) quantitySpinner.getValue();
 
-            Object selectedObject = (Object)listOfProducts.getSelectedValue();
-            Product selectedProduct = (Product)selectedObject;
+            Product selectedProduct = listOfProducts.getSelectedValue();
 
             OrderLine orderLine = new OrderLine(selectedProduct, quantity);
             Order order = customer.getCurrentOrder();
@@ -118,17 +81,25 @@ public class LandingPage {
         }
     }
 
-    protected ImageIcon createImageIcon(String path,
-                                        String description) {
-        java.net.URL imgURL = getClass().getResource(path);
-        if (imgURL != null) {
-            return new ImageIcon(imgURL, description);
-        } else {
-            System.err.println("Couldn't find file: " + path);
-            return null;
-        }
+    private HashMap<Integer, Product> getAllProducts() {
+        Product productA = new Product(0,"Product A","Description A", BigDecimal.valueOf(19.99), 5, "src/main/resources/product.images/derek_crocs.png");
+        Product productB = new Product(0,"Product B","Description B", BigDecimal.valueOf(29.99), 10, "src/main/resources/product.images/derek_snowglobe.png");
+
+        HashMap<Integer, Product> productMap = new HashMap<>(); //set this to the DB read function
+        productMap.put(0, productA);
+        productMap.put(1, productB);
+        return productMap;
     }
 
+    private void displayProductList(HashMap<Integer, Product> productMap, DefaultListModel<Product> listModel) {
+        for (Map.Entry<Integer, Product> productEntry : productMap.entrySet()) {
+            Product product = productEntry.getValue();
+            if (product.getStockLevel() > 0 ){
+                listModel.addElement(product);
+            }
+        }
+        listOfProducts.setModel(listModel);
+    }
 
     public static void main(String[] args) {
         Customer customer = new Customer();
