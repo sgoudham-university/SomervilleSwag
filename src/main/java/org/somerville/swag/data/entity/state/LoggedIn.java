5b14105f7abf3426d62e3cfe091e1ea9845cf4ef
Hammy <sgoudham@gmail.com>
2021-03-24 20:51:23 +0000

    dev/customer: Introduce logging and update method signatures

--- src/main/java/org/somerville/swag/data/entity/state/LoggedIn.java
+++ src/main/java/org/somerville/swag/data/entity/state/LoggedIn.java
@@ -2,36 +2,46 @@ package org.somerville.swag.data.entity.state;
 
 import org.somerville.swag.data.entity.Customer;
 import org.somerville.swag.data.entity.CustomerState;
+import org.somerville.swag.data.entity.OrderLine;
+import org.somerville.swag.data.entity.Product;
+import org.somerville.swag.data.service.LoggingService;
+import org.somerville.swag.data.service.LoggingServiceImpl;
 
 import javax.swing.*;
+import java.util.List;
 import java.util.Objects;
 
 public class LoggedIn implements CustomerState {
 
-    private Customer customer;
+    private final Customer customer;
+
+    private LoggingService loggingService = LoggingServiceImpl.getInstance();
 
     public LoggedIn(Customer customer) {
         this.customer = customer;
     }
 
     @Override
-    public void signUp(JPanel root, String... guestData) {
-        //TODO throw error mesage
+    public void signUp(JPanel root, List<String> guestData) {
+        JOptionPane.showMessageDialog(root, "Uh Oh! Can't Sign Up When Logged In!",
+                "Sign Up Swag-no", JOptionPane.ERROR_MESSAGE);
     }
 
     @Override
     public void logIn(JPanel root, String email, String password) {
-        JOptionPane.showMessageDialog(root, "Cant Log in when you're already logged in",
+        JOptionPane.showMessageDialog(root, "Uh Oh! Can't Log In When Logged In!",
                 "Log In Swag-no", JOptionPane.ERROR_MESSAGE);
     }
 
     @Override
     public void logOut(JPanel root) {
-        //customer = null
-        // TODO implement refresh method && change state.
         customer.changeCustomerState(new Guest(customer));
+
+        // TODO implement refresh method
+
         JOptionPane.showMessageDialog(root, "Logged out Success. D-money will miss you",
                 "Logged out", JOptionPane.INFORMATION_MESSAGE);
+        loggingService.logCustomerLoggedOut(customer.getCustomerId());
     }
 
     @Override
@@ -40,18 +50,22 @@ public class LoggedIn implements CustomerState {
     }
 
     @Override
-    public void addProductToBasket() {
+    public void addProductToBasket(Product product, int quantity) {
 
     }
 
     @Override
-    public void removeProductFromBasket() {
+    public void removeProductFromBasket(OrderLine orderLine) {
 
     }
 
     @Override
-    public void purchaseItems() {
+    public void purchaseItems(JPanel root) {
 
+    }
+
+    public void setLoggingService(LoggingService loggingService) {
+        this.loggingService = loggingService;
     }
 
     @Override
