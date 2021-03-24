73402ca8838d324ad4dfc1a538c5a11a4747882f
Hammy <sgoudham@gmail.com>
2021-03-24 01:42:56 +0000

    [A1][C1] dev/database: Add setter for loggingService in interface

--- src/main/java/org/somerville/swag/data/source/DBExecute.java
+++ src/main/java/org/somerville/swag/data/source/DBExecute.java
@@ -1,10 +1,13 @@
 package org.somerville.swag.data.source;
 
 import org.somerville.swag.data.exception.SQLStatementException;
+import org.somerville.swag.data.service.LoggingService;
 
 import java.sql.ResultSet;
 
 public interface DBExecute {
     ResultSet executeSelect(String selectQuery) throws SQLStatementException;
     void executeInsert(String insertStatement) throws SQLStatementException;
+
+    void setLoggingService(LoggingService loggingService);
 }
