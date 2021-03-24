53724a5913cb8da4294884cfb638fa7fa2df67e5
Alan Robinson <alanrobinson1337@gmail.com>
2021-03-24 14:07:08 +0000

    [E1][F1] dev/productDisplay add comments to registration verification

--- src/main/java/org/somerville/swag/display/SignUp.java
+++ src/main/java/org/somerville/swag/display/SignUp.java
@@ -36,14 +36,14 @@ public class SignUp {
         });
 
         confirmButton.addActionListener(actionEvent -> {
-            if(!txtFirstName.getText().strip().matches("[a-zA-Z]{1,15}") ||
-                    !txtLastName.getText().strip().matches("[a-zA-Z]{1,15}") ||
-                    !txtEmail.getText().strip().matches("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$") ||
-                    !String.valueOf(txtPassword.getPassword()).matches("(^?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8}$") ||
-                    !String.valueOf(txtPasswordConfirm.getPassword()).matches("(^?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8}$") ||
-                    !txtAddress1.getText().matches("^[a-zA-Z0-9_ ]*$") ||
-                    !txtPhoneNo.getText().strip().matches("[0-9]{11}") &&
-                    !String.valueOf(txtPassword.getPassword()).equals(String.valueOf(txtPasswordConfirm.getPassword()))
+            if(!txtFirstName.getText().strip().matches("[a-zA-Z]{1,15}") || //only characters, min length 1 - max length 15
+                    !txtLastName.getText().strip().matches("[a-zA-Z]{1,15}") || //only characters, min length 1 - max length 15
+                    !txtEmail.getText().strip().matches("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$") || //Simple email expression. Doesn't allow numbers in the domain name and doesn't allow for top level domains that are less than 2 or more than 3 letters (which is fine until they allow more). Doesn't handle multiple &quot;.&quot; in the domain (joe@abc.co.uk).
+                    !String.valueOf(txtPassword.getPassword()).matches("^[a-zA-Z]\\w{3,14}$") || //The password's first character must be a letter, it must contain at least 4 characters and no more than 15 characters and no characters other than letters, numbers and the underscore may be use
+                    !String.valueOf(txtPasswordConfirm.getPassword()).matches("^[a-zA-Z]\\w{3,14}$") || //The password's first character must be a letter, it must contain at least 4 characters and no more than 15 characters and no characters other than letters, numbers and the underscore may be used
+                    !txtAddress1.getText().matches("^[a-zA-Z0-9_ ]*$") || //no special characters, whisepace premitted
+                    !txtPhoneNo.getText().strip().matches("[0-9]{11}") && //only numbers,, 11 characters
+                    !String.valueOf(txtPassword.getPassword()).equals(String.valueOf(txtPasswordConfirm.getPassword())) //both password fields must match
             )   //order of pressidence/logic for and and or inside the same ifs???
             {
                 JOptionPane.showMessageDialog(root, "Something went wrong and the code is so bad I dont know what",
