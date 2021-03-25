2c38153b9f5b092ba058f8147d4e6a45e97b2e13
Hammy <sgoudham@gmail.com>
2021-03-25 12:40:37 +0000

    dev/customer: Stub out tests for LoggedIn State

---
+++ src/test/java/org/somerville/swag/data/entity/state/LoggedInTest.java
@@ -0,0 +1,42 @@
+package org.somerville.swag.data.entity.state;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LoggedInTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void failToSignUp() {
+
+    }
+
+    @Test
+    void failToLogIn() {
+
+    }
+
+    @Test
+    void successfullyLogOut() {
+
+    }
+
+    @Test
+    void successfullyAddProductToBasket() {
+
+    }
+
+    @Test
+    void successfullyRemoveProductFromBasket() {
+
+    }
+
+    @Test
+    void successfullyPurchaseProducts() {
+
+    }
+}
