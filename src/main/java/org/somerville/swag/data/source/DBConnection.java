579f142e5c585e9d998acee2d9dd7624995a7d27
Hammy <sgoudham@gmail.com>
2021-03-27 02:10:03 +0000

    dev/refactor: Update interface

--- src/main/java/org/somerville/swag/data/source/DBConnection.java
+++ src/main/java/org/somerville/swag/data/source/DBConnection.java
@@ -1,10 +1,14 @@
 package org.somerville.swag.data.source;
 
 import org.somerville.swag.data.exception.SQLConnectionException;
+import org.somerville.swag.data.service.LoggingService;
 
 import java.sql.Connection;
 
 public interface DBConnection {
     Connection connect(String databaseUrl) throws SQLConnectionException;
     Connection connect() throws SQLConnectionException;
+
+    void setDatabaseUrl(String databaseUrl);
+    void setLoggingService(LoggingService loggingService);
 }
